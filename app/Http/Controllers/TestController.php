6e99150f4cb39d776a6812eb4d7e6c748ef888ac
chen <2680283850@qq.com>
2020-11-13 09:38:06 +0800

    41231

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -12,7 +12,10 @@ use GuzzleHttp\Client;
 class TestController extends Controller
 {
   public function index(){
-        $this->checkSignature();
+        $res=$this->checkSignature();
+        if ($res) {
+            echo $_GET['echostr'];
+        }
         // $res=$this->text();
           //创建菜单
         // $res1=$this->create_moun();
